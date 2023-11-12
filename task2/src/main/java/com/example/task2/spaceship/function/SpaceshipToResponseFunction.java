650fa5bcc13612b751f800e712c2b46c5fdecde7
iben0402 <iwona.bbbb@gmail.com>
2023-11-12 09:53:40 +0100

    spaceship/s to response functions made

--- task2/src/main/java/com/example/task2/spaceship/function/SpaceshipToResponseFunction.java
+++ task2/src/main/java/com/example/task2/spaceship/function/SpaceshipToResponseFunction.java
@@ -1,4 +1,20 @@
 package com.example.task2.spaceship.function;
 
-public class SpaceshipToResponseFunction {
+import com.example.task2.spaceship.dto.GetSpaceshipResponse;
+import com.example.task2.spaceship.entity.Spaceship;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class SpaceshipToResponseFunction implements Function<Spaceship, GetSpaceshipResponse> {
+
+    @Override
+    public GetSpaceshipResponse apply(Spaceship spaceship) {
+        return GetSpaceshipResponse.builder()
+                .ID(spaceship.getID())
+                .name(spaceship.getName())
+                .capacity(spaceship.getCapacity())
+                .build();
+    }
 }
--- task2/src/main/java/com/example/task2/spaceship/function/SpaceshipsToResponseFunction.java
+++ task2/src/main/java/com/example/task2/spaceship/function/SpaceshipsToResponseFunction.java
@@ -1,4 +1,25 @@
 package com.example.task2.spaceship.function;
 
-public class SpaceshipsToResponseFunction {
+import com.example.task2.spaceship.dto.GetSpaceshipsResponse;
+import com.example.task2.spaceship.entity.Spaceship;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Component
+public class SpaceshipsToResponseFunction implements Function<List<Spaceship>, GetSpaceshipsResponse>{
+
+    @Override
+    public GetSpaceshipsResponse apply(List<Spaceship> entities) {
+        return GetSpaceshipsResponse.builder()
+                .spaceships(entities.stream()
+                        .map(spaceship -> GetSpaceshipsResponse.Spaceship.builder()
+                                .ID(spaceship.getID())
+                                .name(spaceship.getName())
+                                .build())
+                        .toList())
+                .build();
+    }
+
 }
