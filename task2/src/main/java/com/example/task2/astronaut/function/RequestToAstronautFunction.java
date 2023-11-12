40f7f9d7f38c4951328f983ed71c46ca7d6fac8f
iben0402 <iwona.bbbb@gmail.com>
2023-11-12 09:06:20 +0100

    added empty functions for requests and responses

---
+++ task2/src/main/java/com/example/task2/astronaut/function/RequestToAstronautFunction.java
@@ -0,0 +1,4 @@
+package com.example.task2.astronaut.function;
+
+public class RequestToAstronautFunction {
+}
---
+++ task2/src/main/java/com/example/task2/spaceship/function/SpaceshipsToResponseFunction.java
@@ -0,0 +1,4 @@
+package com.example.task2.spaceship.function;
+
+public class SpaceshipsToResponseFunction {
+}
