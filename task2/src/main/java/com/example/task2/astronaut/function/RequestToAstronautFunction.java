58828f9adbbd60a949f318cc50b427052b0d2201
iben0402 <iwona.bbbb@gmail.com>
2023-11-12 10:03:41 +0100

    update and create astronauts functions for requests

--- task2/src/main/java/com/example/task2/astronaut/function/RequestToAstronautFunction.java
+++ task2/src/main/java/com/example/task2/astronaut/function/RequestToAstronautFunction.java
@@ -1,4 +1,24 @@
 package com.example.task2.astronaut.function;
 
-public class RequestToAstronautFunction {
+import com.example.task2.astronaut.dto.PutAstronautRequest;
+import com.example.task2.astronaut.entity.Astronaut;
+import com.example.task2.spaceship.entity.Spaceship;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.function.BiFunction;
+
+@Component
+public class RequestToAstronautFunction implements BiFunction<UUID, PutAstronautRequest, Astronaut> {
+    @Override
+    public Astronaut apply(UUID id, PutAstronautRequest request) {
+        return Astronaut.builder()
+                .ID(id)
+                .name(request.getName())
+                .yearOfBirth(request.getYearOfBirth())
+                .spaceship(Spaceship.builder()
+                        .ID(request.getSpaceship())
+                        .build())
+                .build();
+    }
 }
--- task2/src/main/java/com/example/task2/astronaut/function/UpdateAstronautWithRequestFunction.java
+++ task2/src/main/java/com/example/task2/astronaut/function/UpdateAstronautWithRequestFunction.java
@@ -1,4 +1,17 @@
 package com.example.task2.astronaut.function;
 
-public class UpdateAstronautWithRequestFunction {
+import com.example.task2.astronaut.dto.PatchAstronautRequest;
+import com.example.task2.astronaut.entity.Astronaut;
+
+import java.util.function.BiFunction;
+
+public class UpdateAstronautWithRequestFunction implements BiFunction<Astronaut, PatchAstronautRequest, Astronaut> {
+    @Override
+    public Astronaut apply(Astronaut entity, PatchAstronautRequest request) {
+        return Astronaut.builder()
+                .ID(entity.getID())
+                .name(request.getName())
+                .yearOfBirth(request.getYearOfBirth())
+                .build();
+    }
 }
