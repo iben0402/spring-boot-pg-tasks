6c655c4983f4af953fcd22831e6ae5b8f974a4bf
iben0402 <iwona.bbbb@gmail.com>
2023-11-12 09:48:32 +0100

    astronaut/s to response functions made

--- task2/src/main/java/com/example/task2/astronaut/function/AstronautToResponseFunction.java
+++ task2/src/main/java/com/example/task2/astronaut/function/AstronautToResponseFunction.java
@@ -1,4 +1,24 @@
 package com.example.task2.astronaut.function;
 
-public class AstronautToResponseFunction {
+import com.example.task2.astronaut.dto.GetAstronautResponse;
+import com.example.task2.astronaut.entity.Astronaut;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class AstronautToResponseFunction  implements Function<Astronaut, GetAstronautResponse> {
+    @Override
+    public GetAstronautResponse apply(Astronaut astronaut) {
+        return GetAstronautResponse.builder()
+                .ID(astronaut.getID())
+                .name(astronaut.getName())
+                .yearOfBirth(astronaut.getYearOfBirth())
+                .spaceship(GetAstronautResponse.Spaceship.builder()
+                        .ID(astronaut.getSpaceship().getID())
+                        .name(astronaut.getSpaceship().getName())
+                        .capacity(astronaut.getSpaceship().getCapacity())
+                        .build())
+                .build();
+    }
 }
--- task2/src/main/java/com/example/task2/astronaut/function/AstronautsToResponseFunction.java
+++ task2/src/main/java/com/example/task2/astronaut/function/AstronautsToResponseFunction.java
@@ -1,4 +1,25 @@
 package com.example.task2.astronaut.function;
 
-public class AstronautsToResponseFunction {
+import com.example.task2.astronaut.dto.GetAstronautResponse;
+import com.example.task2.astronaut.dto.GetAstronautsResponse;
+import com.example.task2.astronaut.entity.Astronaut;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Component
+public class AstronautsToResponseFunction implements Function<List<Astronaut>, GetAstronautsResponse> {
+    @Override
+    public GetAstronautsResponse apply(List<Astronaut> entities) {
+        return GetAstronautsResponse.builder()
+                .astronauts(entities.stream()
+                        .map(astronaut -> GetAstronautsResponse.Astronaut.builder()
+                                .ID(astronaut.getID())
+                                .name(astronaut.getName())
+                                .build())
+                        .toList())
+                .build();
+    }
+
 }
