adfa33da28cd1ac967bd0dd9b9ff57436656601f
iben0402 <iwona.bbbb@gmail.com>
2023-11-12 10:22:32 +0100

    astronauts controller interface done

--- src/main/java/com/example/task2/astronaut/controller/AstronautController.java
+++ src/main/java/com/example/task2/astronaut/controller/AstronautController.java
@@ -1,4 +1,43 @@
 package com.example.task2.astronaut.controller;
 
+import com.example.task2.astronaut.dto.GetAstronautsResponse;
+import com.example.task2.astronaut.dto.PutAstronautRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
 public interface AstronautController {
+    @GetMapping("/api/astronauts")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    GetAstronautsResponse getAstronauts();
+
+    @GetMapping("/api/spaceships/{spaceshipID}/astronauts")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    GetAstronautsResponse getSpaceshipsAstronauts(
+            @PathVariable("spaceshipID") UUID spaceshipID
+    );
+
+    @GetMapping("/api/astronauts/{ID}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    GetAstronautsResponse getAstronaut(
+            @PathVariable("ID") UUID ID
+    );
+
+    @PutMapping("/api/astronauts/{ID}")
+    @ResponseStatus(HttpStatus.CREATED)
+    void putAstronaut(
+            @PathVariable("ID") UUID ID,
+            @RequestBody PutAstronautRequest request
+    );
+
+    @DeleteMapping("/api/astronauts/{ID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteAstronaut(
+            @PathVariable("ID") UUID ID
+    );
+
 }
